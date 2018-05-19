91e34009bf4fdfd9aad92865c1732ede99f52f43
Haoyu Zhang <haoyuz@cs.princeton.edu>
2018-05-18 23:50:59 -0400

    Updated email address

--- js/theme.js
+++ js/theme.js
@@ -1,5 +1,7 @@
 // initialized with jQuery
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();   
+    estr = "me" + "@" + "haoyuzhang" + "." + "org";
+    $('span.estr').html("<a href='" + "mai" + "lto:" + estr + "'>" + estr + "</a>");
 });
 
