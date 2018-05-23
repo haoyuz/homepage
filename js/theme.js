d87773702216f6a3076a954afb6edcca0749d0fc
Haoyu Zhang <haoyuz@outlook.com>
2018-05-23 17:21:22 -0400

    Removed mailto link from email address

--- js/theme.js
+++ js/theme.js
@@ -2,6 +2,6 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();   
     estr = "me" + "@" + "haoyuzhang" + "." + "org";
-    $('span.estr').html("<a href='" + "mai" + "lto:" + estr + "'>" + estr + "</a>");
+    $('span.estr').html("<span class='text-info'>" + estr + "</a>");
 });
 
